00439ef8adf53c8045d8ddbfd655d0ccb2cab1ac
David Lattimore <dvdlttmr@gmail.com>
2023-05-05 18:59:03 +1000

    Integration test shouldn't be interactive

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,6 +8,7 @@ fn integration_test() -> Result<()> {
     let crate_root = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let cackle_exe = target_dir().join("cackle");
     let status = Command::new(&cackle_exe)
+        .arg("--non-interactive")
         .arg("--path")
         .arg(crate_root.join("test_crates"))
         .status()
