dd8132e0073390309128aa44faad431305983597
David Lattimore <dvdlttmr@gmail.com>
2023-07-06 17:08:17 +1000

    Increase debug level to 2 so that we have information about variables

--- src/proxy/cargo.rs
+++ src/proxy/cargo.rs
@@ -16,13 +16,15 @@ pub(crate) fn command(base_command: &str, dir: &Path, args: &Args) -> Command {
         .arg("--config")
         .arg(format!("profile.{PROFILE_NAME}.inherits=\"dev\""));
     // We need debug information so that we know where code came from and can attribute symbol
-    // references to a particular crate.
+    // references to a particular crate. Level 1 is sufficient for code within functions, but we
+    // need level 2 in order to have debug information for variables.
+    let debug_level = 2;
     command
         .arg("--config")
-        .arg(format!("profile.{PROFILE_NAME}.debug=1"));
-    command
-        .arg("--config")
-        .arg(format!("profile.{PROFILE_NAME}.build-override.debug=1"));
+        .arg(format!("profile.{PROFILE_NAME}.debug={debug_level}"));
+    command.arg("--config").arg(format!(
+        "profile.{PROFILE_NAME}.build-override.debug={debug_level}"
+    ));
     // Optimisation would likely make it harder to figure out where code came from.
     command
         .arg("--config")
