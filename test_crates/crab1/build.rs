0bdc1f769f2698ded837d3e2632facee74ab4543
David Lattimore <dvdlttmr@gmail.com>
2023-07-17 17:09:59 +1000

    Allow bypass of network when running tests

--- test_crates/crab1/build.rs
+++ test_crates/crab1/build.rs
@@ -1,8 +1,10 @@
 use std::net::ToSocketAddrs;
 
 fn main() {
-    "rust-lang.org:443"
-        .to_socket_addrs()
-        .expect("Failed to resolve rust-lang.org");
+    if option_env!("CACKLE_TEST_NO_NET").is_none() {
+        "rust-lang.org:443"
+            .to_socket_addrs()
+            .expect("Failed to resolve rust-lang.org");
+    }
     assert!(crab5::do_something());
 }
