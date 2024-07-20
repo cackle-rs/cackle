d5fd64fbdc14ec5375ebc5af62a65c5a6f998781
David Lattimore <dvdlttmr@gmail.com>
2024-07-20 19:06:55 +1000

    Fix failure on recent nightly

--- test_crates/crab-7/src/lib.rs
+++ test_crates/crab-7/src/lib.rs
@@ -4,7 +4,12 @@ pub fn do_something() {}
 
 /// This function runs before main. Make sure that we detect that it uses filesystem APIs.
 extern "C" fn before_main() {
-    println!("Does / exist?: {:?}", Path::new("/").exists());
+    // We avoid actually calling filesystem APIs before main, since Rust doesn't really support
+    // pre-main activities and with recent versions doing so causes an assertion failure. We can
+    // apparently still get away with the environment variable check.
+    if std::env::var("FOO").is_ok() {
+        println!("Does / exist?: {:?}", Path::new("/").exists());
+    }
 }
 
 #[link_section = ".init_array"]
